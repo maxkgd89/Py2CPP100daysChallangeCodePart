58b29b2dbd821377e9e2f4dcf67f82cd3e2e725b
maxkgd89 <37918183+maxkgd89@users.noreply.github.com>
2025-05-23 14:14:08 +0000

    01_BandNameGenerator

---
+++ .ccls-cache/@home@runner@workspace/other_projects@01_BandNameGenerator.cpp
@@ -0,0 +1,22 @@
+//Band name generator
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string street;
+    string pet;
+
+    cout << "Welcome to the Band Name Generator." << endl;
+
+    cout << "What's the name of the city you grew up in?" << endl;
+    getline(cin, street);  // Using getline to handle spaces in city names
+
+    cout << "What's your pet's name?" << endl;
+    getline(cin, pet);
+
+    cout << "Your band name could be " << street << " " << pet << endl;
+
+    return 0;
+}
